089d2bd8763c882cf2816b546ce20992a08fb220
Michael Dahl <michael@dahl.software>
2024-10-23 17:45:01 +0200

    Add cname and custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://westwood-soft.github.io',
+  site: 'https://westwood.software',
   integrations: [tailwind()],
 });
